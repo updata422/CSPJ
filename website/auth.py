d47c68cabac49f0dfdab589dd700ef23cc5e53e7
updata422 <88725675+updata422@users.noreply.github.com>
2021-08-14 03:18:34 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -80,3 +80,56 @@ def login():
 @auth.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
+
+@auth.route('/api/login', methods=['POST'])
+def api_login():
+    data = request.get_json()
+    user_email = data.get('email')
+    user_password = data.get('password')
+
+    user = db.session.execute(f"SELECT * FROM User WHERE email = '{user_email}' AND password = '{user_password}'").first()
+    uName = getRequestString("username");
+    uPass = getRequestString("userpassword");
+
+    sql = 'SELECT * FROM Users WHERE Name ="' + uName + '" AND Pass ="' + uPass + '"'
+
+
+    if user:
+        return make_response(jsonify({'message': 'Login succesful'}), 200)
+
+    else:
+        if User.query.filter_by(email=user_email).first():
+            return make_response(jsonify({'message': 'Password incorrect.'}), 401)
+
+        elif User.query.filter_by(password=user_password).first():
+            return make_response(jsonify({'message': 'No user with that email.'}), 401)
+
+        else:
+            return make_response(jsonify({'message': 'No user with that email and password'}), 401)
+
+
+@auth.route('/api/sign-up', methods=['POST'])
+def api_sign_up():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user or len(email) < 4:
+        return make_response(jsonify({'message': 'Sign up form invalid.'}), 401)
+    else:
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'New user created.'}), 201)
+
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
+
